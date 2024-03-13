290ddbc51dceaab006f476b77920236f144c8209
Nicolas Handfield <nicolas.handfield.1@etsmtl.net>
2024-03-13 12:22:32 -0400

    Saving seems to work, more testing needed

--- LLM-bot-extension/src/main.py
+++ LLM-bot-extension/src/main.py
@@ -2,23 +2,30 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, GPTNeoForCausalLM, GPT2Tokenizer
 from torch import cuda
 
 from uuid import uuid4
 from pydantic import BaseModel
 
 import time
+import logging
+import os.path
+
 
 device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
 model_id = "codellama/CodeLlama-7b-Instruct-hf"
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 # tokenizer = AutoTokenizer.from_pretrained(model_id)
 # model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype="auto", device_map="auto")
 #test
 tokenizer = ""
 model = ""
 
-save_dir = "/models"
+save_dir = "/models/"
 
 historique = {}
 
@@ -37,6 +44,20 @@ class PromtRequest(BaseModel):
     id: str
     promt: str
 
+def get_model_and_tokenizer(model_id, auto_model, auto_tokenizer):
+    if(os.path.exists(save_dir + model_id)):
+        logger.info("Loading model and tokenizer from local files")
+        model = auto_model.from_pretrained(save_dir + model_id)
+        tokenizer = auto_tokenizer.from_pretrained(save_dir + model_id)
+        logger.info("Loading from local files complete.")
+    else:
+        logger.info("Model and tokenizer not found locally. Downloading...")
+        model = auto_model.from_pretrained(model_id)
+        tokenizer = auto_tokenizer.from_pretrained(model_id)
+        logger.info("Download complete. Saving models to volume " + save_dir + model_id)
+        model.save_pretrained(save_dir + model_id)
+        tokenizer.save_pretrained(save_dir + model_id)
+        logger.info("Successfully saved model and tokenizer to volume.")
 
 @app.get("/")
 def read_root():
@@ -48,11 +69,7 @@ def premier_demarrage():
     global model
     global tokenizer
 
-    model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype="auto", device_map="auto")
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-
-    tokenizer.save_pretrained(save_dir)
-    model.save_pretrained(save_dir)
+    get_model_and_tokenizer(model_id, AutoModelForCausalLM, AutoTokenizer)
     print(device)
     return {"Page": "Premier demarrage"}
 
@@ -131,3 +148,51 @@ def test_generate():
     print(tokenizer.decode(output))
     print('--- %s secondes ---' % (time.time() - start_time))
     return {"result": tokenizer.decode(output)}
+
+@app.get("/testsavebasic")
+def test_save():
+    test_file = open(save_dir + 'testfile.txt', 'w')
+    test_file.write('Hello world')
+    logger.info(str(os.path.abspath(test_file.name)))
+    test_file.close()
+
+@app.get("/testreadbasic")
+def test_save():
+    test_file = open(save_dir + 'testfile.txt', 'r')
+    logger.info("The file reads : " + test_file.read())
+    logger.info(os.path.abspath(test_file.name))
+    test_file.close()
+
+@app.get("/testsaveadvanced")
+def test_save():
+    model_id = "NlpHUST/gpt-neo-vi-small"
+    if(os.path.exists(save_dir + model_id)):
+        logger.info("Loading model and tokenizer from local files")
+        model = GPTNeoForCausalLM.from_pretrained(save_dir + model_id)
+        tokenizer = GPT2Tokenizer.from_pretrained(save_dir + model_id)
+        logger.info("Loading from local files complete.")
+    else:
+        logger.info("Model and tokenizer not found locally. Downloading...")
+        model = GPTNeoForCausalLM.from_pretrained(model_id)
+        tokenizer = GPT2Tokenizer.from_pretrained(model_id)
+        logger.info("Download complete. Saving models to volume " + save_dir + model_id)
+        model.save_pretrained(save_dir + model_id)
+        tokenizer.save_pretrained(save_dir + model_id)
+        logger.info("Successfully saved model and tokenizer to volume.")
+    
+@app.get("/testsavefinal")
+def test_save():
+    get_model_and_tokenizer("NlpHUST/gpt-neo-vi-small", GPTNeoForCausalLM, GPT2Tokenizer)
+
+    start_time = time.time()
+
+    prompt = """
+    xin chào
+    """
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    gen_tokens = model.generate(input_ids, do_sample=True, temperature=1.0, max_length=1024)
+    output = tokenizer.batch_decode(gen_tokens)[0]
+    logger.info(output)
+
+    logger.info('--- %s secondes ---' % (time.time() - start_time))
+    return {"result": output}
